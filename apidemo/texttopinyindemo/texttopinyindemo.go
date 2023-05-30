0e74fdcc91363b0bfbdf0ddd7c61c6d39ab031bc
lingbo <lingbo@rd.netease.com>
2023-05-30 16:12:59 +0800

    新增文本转拼音demo & 更新tts请求参数

--- apidemo/texttopinyindemo/texttopinyindemo.go
+++ apidemo/texttopinyindemo/texttopinyindemo.go
@@ -11,9 +11,6 @@ var appKey = ""
 // 您的应用密钥
 var appSecret = ""
 
-// 合成音频保存路径, 例windows路径：PATH = "C:\\tts\\media.mp3"
-var path = ""
-
 func main() {
 	// 添加请求参数
 	paramsMap := createRequestParams()
@@ -23,13 +20,11 @@ func main() {
 	// 添加鉴权相关参数
 	authv3.AddAuthParams(appKey, appSecret, paramsMap)
 	// 请求api服务
-	result := utils2.DoPost("https://openapi.youdao.com/ttsapi", header, paramsMap, "audio")
+	result := utils2.DoPost("https://openapi.youdao.com/getPinYin", header, paramsMap, "application/json")
 	// 打印返回结果
 	if result != nil {
-		utils2.SaveFile(path, result, false)
-		print("save file path: " + path)
+		fmt.Print(string(result))
 	}
-
 }
 
 func createRequestParams() map[string][]string {
@@ -38,13 +33,9 @@ func createRequestParams() map[string][]string {
 		note: 将下列变量替换为需要请求的参数
 		取值参考文档: https://ai.youdao.com/DOCSIRMA/html/%E8%AF%AD%E9%9F%B3%E5%90%88%E6%88%90TTS/API%E6%96%87%E6%A1%A3/%E8%AF%AD%E9%9F%B3%E5%90%88%E6%88%90%E6%9C%8D%E5%8A%A1/%E8%AF%AD%E9%9F%B3%E5%90%88%E6%88%90%E6%9C%8D%E5%8A%A1-API%E6%96%87%E6%A1%A3.html
 	*/
-	q := "待合成文本"
-	voiceName := "发言人名称"
-	format := "mp3"
+	q := "待获取拼音的文本"
 
 	return map[string][]string{
 		"q":        {q},
-		"voiceName": {voiceName},
-		"format":   {format},
 	}
 }
